bb9b1e55162e6b6b40b24e588e37dfa3dfe6a4a8
John Smith <John.Smith@example.com>
2019-06-19 15:32:16 -0300

    Removing starts

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -1,8 +1,8 @@
-dunst &
+#dunst &
 nitrogen --restore &
-$HOME/.config/i3/monitor/saver.sh &
-killall conky
-/home/alexvanaxe/.config/i3/conky/conky.sh &
-compton -b &
-killall dwmstatusbar
-~/.dwm/dwmstatusbar &
+#$HOME/.config/i3/monitor/saver.sh &
+#killall conky
+#/home/alexvanaxe/.config/i3/conky/conky.sh &
+#compton -b &
+#killall dwmstatusbar
+#~/.dwm/dwmstatusbar &
