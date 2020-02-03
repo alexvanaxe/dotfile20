210d4765d15a39669c25ddeb80f10fc067d3309b
John Smith <John.Smith@example.com>
2020-02-03 13:43:48 -0300

    Teste

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -4,11 +4,14 @@ $HOME/.config/i3/monitor/saver.sh &
 killall conky
 killall stalonetray &
 killall dwmstatusbar
+killall clipster
+killall picom
 sleep 2
 #/home/alexvanaxe/.config/i3/conky/conky.sh &
-#/home/alexvanaxe/.config/conky/night/conky.sh
-/home/alexvanaxe/.config/conky/day/conky.sh
+/home/alexvanaxe/.config/conky/night/conky.sh
+#/home/alexvanaxe/.config/conky/day/conky.sh
 #/home/alexvanaxe/.config/conky/tonight/conky.sh
-compton -b &
+picom -b &
 ~/.dwm/dwmstatusbar &
 stalonetray &
+clipster -d &
--- bin/teste.sh
+++ bin/teste.sh
@@ -1,9 +1,1 @@
-#!/bin/sh
-
-teste=($(playerctl -a metadata 2> /dev/null | awk '{print $1}' | uniq))
-
-teste_len=${#teste[@]}
-
-if [ $teste_len -gt 1 ]; then
-    echo "plus"
-fi
+echo teste
